2b689d200df6d6e8c972d3b8a7a76063469aedf4
EvertonFernandesJr <evertonfernandesjr@gmail.com>
2025-01-06 23:11:59 -0300

    Right side butttons

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -15,11 +15,117 @@ import { DetectionComponent } from '../detection/components/detection/detection.
         <div class="divider divider-horizontal"></div>
         <div
           class="border-2 border-orange-400 h-full w-full md:w-1/2 flex items-center justify-center p-0 md:p-2 overflow-none rounded-box">
-          Conteúdo 2
+          <!--Inicio -->
+          <div class="main-container">
+            <div class="container">
+              <span class="titulo">Gráficos</span>
+            </div>
+            <div class="container2">
+              <span class="titulo2">Relatórios</span>
+            </div>
+            <div class="container3">
+              <span class="titulo3">Ordem serviço</span>
+            </div>
+            <div class="container4">
+              <span class="titulo4">Outros</span>
+            </div>
+          </div>
+          <!--Fim-->
         </div>
       </div>
     </div>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
+  styles: `
+    .main-container{
+      display: grid;
+      grid-template-columns:repeat(auto-fill,minmax(200px, 1fr));
+      gap: 20px;
+      padding: 20px;
+    }
+    .container{
+    display: block;
+    margin: 10px;
+    padding: 10px;
+    border: 4px solid #00A96E;
+    border-radius: 10px;
+    width: 100%;
+    background-color: #00A96E;
+    transition: filter 0.3s ease;
+    }
+    .titulo{
+      color: Black;
+      font-size: 20px;
+      font-weight: bold;
+      text-align: center;
+    }
+    .container:hover{
+      filter: brightness(0.8);
+
+    }
+    .container2{
+    display: block;
+    margin: 10px;
+    padding: 10px;
+    border: 4px solid #00A96E;
+    border-radius: 10px;
+    width: 100%;
+    background-color: #00A96E;
+    transition: filter 0.3s ease;
+    }
+    .titulo2{
+      color: Black;
+      transition: filter 0.3s ease;;
+      font-size: 20px;
+      font-weight: bold;
+      text-align: center;
+    }
+    .container2:hover{
+      filter: brightness(0.8);
+
+    }
+    .container3{
+    display: block;
+    margin: 10px;
+    padding: 10px;
+    border: 4px solid #00A96E;
+    border-radius: 10px;
+    width: 100%;
+    background-color: #00A96E;
+    transition: filter 0.3s ease;
+    }
+    .titulo3{
+      color: Black;
+      transition: filter 0.3s ease;;
+      font-size: 20px;
+      font-weight: bold;
+      text-align: center;
+    }
+    .container3:hover{
+      filter: brightness(0.8);
+
+    }
+    .container4{
+    display: block;
+    margin: 10px;
+    padding: 10px;
+    border: 4px solid #00A96E;
+    border-radius: 10px;
+    width: 100%;
+    background-color: #00A96E;
+    transition: filter 0.3s ease;
+    }
+    .titulo4{
+      color: Black;
+      transition: filter 0.3s ease;;
+      font-size: 20px;
+      font-weight: bold;
+      text-align: center;
+    }
+    .container4:hover{
+      filter: brightness(0.8);
+
+    }
+  `
 })
 export class HomeComponent {}
