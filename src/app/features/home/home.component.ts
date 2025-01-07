e5fca0feefbfbfdf152b106f16b630904e6c093d
EvertonFernandesJr <evertonfernandesjr@gmail.com>
2025-01-07 11:15:00 -0300

    New button animations and colors

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -13,24 +13,23 @@ import { DetectionComponent } from '../detection/components/detection/detection.
           <app-detection />
         </div>
         <div class="divider divider-horizontal"></div>
-        <div
-          class="border-2 border-orange-400 h-full w-full md:w-1/2 flex items-center justify-center p-0 md:p-2 overflow-none rounded-box">
-          <!--Inicio -->
-          <div class="main-container">
-            <div class="container">
-              <span class="titulo">Gráficos</span>
+        <div class="border-2 border-orange-400 h-full w-full md:w-1/2 flex items-center justify-center p-0 md:p-2 overflow-none rounded-box">
+            <!--Inicio -->
+            <div class="main-container">
+              <div class="container">
+                <span class="titulo">Gráficos</span>
+              </div>
+              <div class="container2">
+                <span class="titulo2">Relatórios</span>
+              </div>
+              <div class="container3">
+                <span class="titulo3">Ordem serviço</span>
+              </div>
+              <div class="container4">
+                <span class="titulo4">Outros</span>
+              </div>
             </div>
-            <div class="container2">
-              <span class="titulo2">Relatórios</span>
-            </div>
-            <div class="container3">
-              <span class="titulo3">Ordem serviço</span>
-            </div>
-            <div class="container4">
-              <span class="titulo4">Outros</span>
-            </div>
-          </div>
-          <!--Fim-->
+            <!--Fim-->
         </div>
       </div>
     </div>
@@ -38,8 +37,10 @@ import { DetectionComponent } from '../detection/components/detection/detection.
   changeDetection: ChangeDetectionStrategy.OnPush,
   styles: `
     .main-container{
-      display: grid;
-      grid-template-columns:repeat(auto-fill,minmax(200px, 1fr));
+      border: 400px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
       gap: 20px;
       padding: 20px;
     }
@@ -47,11 +48,11 @@ import { DetectionComponent } from '../detection/components/detection/detection.
     display: block;
     margin: 10px;
     padding: 10px;
-    border: 4px solid #00A96E;
+    border: 4px #655D8A;
     border-radius: 10px;
     width: 100%;
-    background-color: #00A96E;
-    transition: filter 0.3s ease;
+    background-color: #655D8A;
+    transition: filter 0.3s ease, transform 0.3s ease;
     }
     .titulo{
       color: Black;
@@ -63,15 +64,18 @@ import { DetectionComponent } from '../detection/components/detection/detection.
       filter: brightness(0.8);
 
     }
+    .container:active{
+      transform: scale(0.9);
+    }
     .container2{
     display: block;
     margin: 10px;
     padding: 10px;
-    border: 4px solid #00A96E;
+    border: 4px #7897AB;
     border-radius: 10px;
     width: 100%;
-    background-color: #00A96E;
-    transition: filter 0.3s ease;
+    background-color: #7897AB;
+    transition: filter 0.3s ease, transform 0.3s ease;
     }
     .titulo2{
       color: Black;
@@ -84,15 +88,18 @@ import { DetectionComponent } from '../detection/components/detection/detection.
       filter: brightness(0.8);
 
     }
+    .container2:active{
+      transform: scale(0.9);
+    }
     .container3{
     display: block;
     margin: 10px;
     padding: 10px;
-    border: 4px solid #00A96E;
+    border: 4px #D885A3;
     border-radius: 10px;
     width: 100%;
-    background-color: #00A96E;
-    transition: filter 0.3s ease;
+    background-color: #D885A3;
+    transition: filter 0.3s ease, transform 0.3s ease;
     }
     .titulo3{
       color: Black;
@@ -105,15 +112,18 @@ import { DetectionComponent } from '../detection/components/detection/detection.
       filter: brightness(0.8);
 
     }
+    .container3:active{
+      transform: scale(0.9);
+    }
     .container4{
     display: block;
     margin: 10px;
     padding: 10px;
-    border: 4px solid #00A96E;
+    border: 4px solid #FDCEB9;
     border-radius: 10px;
     width: 100%;
-    background-color: #00A96E;
-    transition: filter 0.3s ease;
+    background-color: #FDCEB9;
+    transition: filter 0.3s ease, transform 0.3s ease;
     }
     .titulo4{
       color: Black;
@@ -125,6 +135,9 @@ import { DetectionComponent } from '../detection/components/detection/detection.
     .container4:hover{
       filter: brightness(0.8);
 
+    }
+    .container4:active{
+      transform: scale(0.9);
     }
   `
 })
