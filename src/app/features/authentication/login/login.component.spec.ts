45e16f35e7a7b37e4f060b5a333266ec527332f1
Lucas Peixoto <lspeixotodev@gmail.com>
2025-02-16 22:54:15 -0300

    Update mobile layouts

--- src/app/features/authentication/login/login.component.spec.ts
+++ src/app/features/authentication/login/login.component.spec.ts
@@ -8,6 +8,8 @@ import { NzFlexModule } from 'ng-zorro-antd/flex';
 import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { SupabaseService } from './../../../../app/shared/services/supabase.service';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -16,13 +18,11 @@ describe('LoginComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [NzButtonModule, NzFlexModule, NzInputModule, NzFormModule, ReactiveFormsModule],
-      providers: [SupabaseService],
+      providers: [SupabaseService, HttpClientModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
-
-    fixture.detectChanges();
   });
 
   it('should create', () => {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
+  production: true,
   SUPABASE_URL: 'https://qmhdkjspsnhkwvgodlru.supabase.co',
   SUPABASE_KEY:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFtaGRranNwc25oa3d2Z29kbHJ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk2NDc2MjMsImV4cCI6MjA1NTIyMzYyM30.M-hS8tR584WZtFAmN5Umy5RCNFeAAUMKrmkvZNW05AM',
