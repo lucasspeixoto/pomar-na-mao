d6f48a39dda7a68a100291dbcaf9a4b865756bf0
Lucas Peixoto <lspeixotodev@gmail.com>
2025-03-22 20:15:50 -0300

    Remove cypress from jest tests

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,5 @@ module.exports = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   modulePathIgnorePatterns: ['\\.(css|scss)$'],
-  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/', '/cypress/'],
 };
