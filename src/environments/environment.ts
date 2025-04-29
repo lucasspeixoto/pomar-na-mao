cca11a0c7c2b43e55980ca6096db310056de4d8e
Lucas Peixoto <lspeixotodev@gmail.com>
2025-04-29 09:53:41 -0300

    Include assets primeicons

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  SUPABASE_URL: 'https://cumkqrjwsbyotaojeyxv.supabase.co', //import.meta.env.NG_APP_SUPABASE_URL,
+  SUPABASE_URL: 'https://cumkqrjwsbyotaojeyxv.supabase.co',
   SUPABASE_KEY:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN1bWtxcmp3c2J5b3Rhb2pleXh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU3ODkxMzksImV4cCI6MjA2MTM2NTEzOX0.OtaIAribjEBSZ1cbZkVqxcizYbXy1fCmcGqm4JK0g88', //import.meta.env.NG_APP_SUPABASE_KEY,
-  INDEXDB_KEY: '627bd1e3-0611-41dd-bb5c-e9ef1ed02f1e', //import.meta.env.NG_APP_INDEXDB_KEY,
+  INDEXDB_KEY: '627bd1e3-0611-41dd-bb5c-e9ef1ed02f1e',
 };
