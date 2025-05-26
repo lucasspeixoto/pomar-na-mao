6dd6d9abe0c6ef88e689688a8bff9b8d8fb40f21
Lucas Peixoto <lspeixotodev@gmail.com>
2025-05-25 23:36:44 -0300

    Include auth toolbar missing background color

--- src/app/auth/components/toolbar-services/toolbar-services.component.ts
+++ src/app/auth/components/toolbar-services/toolbar-services.component.ts
@@ -8,24 +8,33 @@ import { RouterLink } from '@angular/router';
   selector: 'app-toolbar-services',
   imports: [Toolbar, AvatarModule, ButtonModule, RouterLink],
   template: `
-    <p-toolbar [style]="{ 'border-radius': '3rem', padding: '0.5rem' }">
+    <p-toolbar
+      [style]="{
+        'border-radius': '3rem',
+        padding: '0.5rem',
+        'background-color': 'var(--primary-color)',
+      }">
       <div class="flex w-full justify-between items-center mx-4 gap-2">
         <!-- Coletar -->
         <div routerLink="/coleta-offline" class="cursor-pointer flex flex-col items-center gap-2">
-          <i class="pi pi-box opacity-80 hover:opacity-100" style="color: rgb(58, 222, 58)"></i>
-          <span class="hover:underline">Coletar</span>
+          <i
+            class="pi pi-box opacity-80 hover:opacity-100"
+            style="font-size: 1.6rem; color: green"></i>
+          <span class="hover:underline font-bold text-black">Coletar</span>
         </div>
         <!-- Detectar -->
         <div class="cursor-pointer flex flex-col items-center gap-2">
-          <i class="pi pi-wifi opacity-80 hover:opacity-100" style="color: rgb(58, 222, 58)"></i>
-          <span class="hover:underline">Detectar</span>
+          <i
+            class="pi pi-wifi opacity-80 hover:opacity-100"
+            style="font-size: 1.6rem; color: green"></i>
+          <span class="hover:underline font-bold text-black">Detectar</span>
         </div>
         <!-- Coletas -->
         <div class="cursor-pointer flex flex-col items-center gap-2">
           <i
             class="pi pi-database opacity-80 hover:opacity-100"
-            style="color: rgb(58, 222, 58)"></i>
-          <span class="hover:underline">Consutar</span>
+            style="font-size: 1.6rem; color: green"></i>
+          <span class="hover:underline font-bold text-black">Consutar</span>
         </div>
       </div>
     </p-toolbar>
