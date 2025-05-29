72c5e037c5ae7ca5f28e74572cfc1d3d02940e8e
Lucas Peixoto <lspeixotodev@gmail.com>
2025-05-29 08:05:23 -0300

    Update login toolbox infos css

--- src/app/auth/components/toolbar-services/toolbar-services.component.ts
+++ src/app/auth/components/toolbar-services/toolbar-services.component.ts
@@ -12,29 +12,25 @@ import { RouterLink } from '@angular/router';
       [style]="{
         'border-radius': '3rem',
         padding: '0.5rem',
-        'background-color': 'var(--primary-color)',
+        'background-color': '#ddd',
       }">
       <div class="flex w-full justify-between items-center mx-4 gap-2">
         <!-- Coletar -->
         <div routerLink="/coleta-offline" class="cursor-pointer flex flex-col items-center gap-2">
-          <i
-            class="pi pi-box opacity-80 hover:opacity-100"
-            style="font-size: 1.2rem; color: green"></i>
-          <span class="hover:underline text-xs text-gray-950">Coletar</span>
+          <i class="pi pi-box text-green-900 hover:font-extrabold" style="font-size: 1.2rem"></i>
+          <span class="hover:underline text-xs sm:text-sm md:text-md text-gray-950">Coletar</span>
         </div>
         <!-- Detectar -->
         <div class="cursor-pointer flex flex-col items-center gap-2">
-          <i
-            class="pi pi-wifi opacity-80 hover:opacity-100"
-            style="font-size: 1.2rem; color: green"></i>
-          <span class="hover:underline font-medium text-gray-950">Detectar</span>
+          <i class="pi pi-wifi text-green-900 hover:font-extrabold" style="font-size: 1.2rem"></i>
+          <span class="hover:underline text-xs sm:text-sm md:text-md text-gray-950">Detectar</span>
         </div>
         <!-- Coletas -->
         <div class="cursor-pointer flex flex-col items-center gap-2">
           <i
-            class="pi pi-database opacity-80 hover:opacity-100"
-            style="font-size: 1.2rem; color: green"></i>
-          <span class="hover:underline font-medium text-gray-950">Consutar</span>
+            class="pi pi-database text-green-900 hover:font-extrabold"
+            style="font-size: 1.2rem"></i>
+          <span class="hover:underline text-xs sm:text-sm md:text-md text-gray-950">Consutar</span>
         </div>
       </div>
     </p-toolbar>
